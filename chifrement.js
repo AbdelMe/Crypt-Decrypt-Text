f705bd5ee55122363fa3d8e99633598c6c796987
AbdelMe <165473623+AbdelMe@users.noreply.github.com>
2024-12-30 11:39:24 +0000

    first commit

---
+++ chifrement.js
@@ -0,0 +1,46 @@
+const decalage = document.getElementById('Number');
+const word = document.getElementById('text');
+const btnEncrypted = document.getElementById('btn');
+const btnDecrypted = document.getElementById('Dechifre'); 
+
+const res = document.getElementById('res');
+
+
+const lst_w = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+btnEncrypted.addEventListener('click', () => {
+    let result = "";
+    for (let letter of word.value) {
+        if (letter >= 'a' && letter <= 'z') {
+            const index = lst_w.indexOf(letter);
+            const newIndex = (index + parseInt(decalage.value)) % 26;
+            result += lst_w[newIndex];
+        } else if (letter >= 'A' && letter <= 'Z') {
+            const index = lst_w.indexOf(letter.toLowerCase());
+            const newIndex = (index + parseInt(decalage.value)) % 26;
+            result += lst_w[newIndex].toUpperCase();
+        } else {
+            result += letter;
+        }
+    }
+    res.innerHTML ='The Text Encrypted Is: ' + result;
+});
+
+btnDecrypted.addEventListener('click', () => {
+    console.log('ee')
+    let result = "";
+    for (let letter of word.value) {
+        if (letter >= 'a' && letter <= 'z') {
+            const index = lst_w.indexOf(letter);
+            const newIndex = (index - parseInt(decalage.value) + 26) % 26; 
+            result += lst_w[newIndex];
+        } else if (letter >= 'A' && letter <= 'Z') {
+            const index = lst_w.indexOf(letter.toLowerCase());
+            const newIndex = (index - parseInt(decalage.value) + 26) % 26;
+            result += lst_w[newIndex].toUpperCase();
+        } else {
+            result += letter;
+        }
+    }
+    res.innerHTML ='The Text Decrypted Is: ' +  result;
+});
